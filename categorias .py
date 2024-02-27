d51afa367a82514b13cdad50a5dd299ef77473b8
himayrob <160776152+himayrob@users.noreply.github.com>
2024-02-27 09:20:51 -0500

    punto de guardado

---
+++ categorias .py
@@ -0,0 +1,47 @@
+menu = dict({
+    "panes-blancos": {
+        "producto":list([
+            {"nombre":"baguete", "valor": 2500},
+            {"nombre":"pan de molde", "valor": 3000}
+            {"nombre":"pan de viena", "valor": 6000},
+            {"nombre":"pan de pueblo", "valor": 4000},
+            {"nombre":"pan de barrra", "valor": 4500},
+            {"nombre":"pan frances", "valor": 7000}
+            {"nombre":}
+        ])
+    }
+    "
+})
+Panes blancos:
+Baguette
+Pan de molde
+Pan de Viena
+Pan de pueblo
+Pan de barra
+Pan francés
+Pan de leche
+Pan de chapata
+Pan de viena
+Pan de hamburguesa
+Panes integrales:
+Pan integral
+Pan de centeno integral
+Pan multicereales
+Pan de espelta integral
+Pan de semillas
+Pan de trigo sarraceno
+Pan de masa madre integral
+Pan de nueces integral
+Pan de avena integral
+Pan de molde integral
+Otros panes especiales:
+Focaccia
+Ciabatta
+Pan de pita
+Bagel
+Pretzel
+Pan de maíz
+Pan de aceitunas
+Pan de ajo
+Pan de pasas y nueces
+Pan de cereales y frutos secos
