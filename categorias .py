166f74e57870cab84cc1d725e93a031fc6a3c3c4
himayrob <160776152+himayrob@users.noreply.github.com>
2024-02-27 13:55:39 -0500

    ya estoy comprendiendo un poco

--- categorias .py
+++ categorias .py
@@ -6,33 +6,42 @@ menu = dict({
             {"nombre":"pan de viena", "valor": 6000},
             {"nombre":"pan de pueblo", "valor": 4000},
             {"nombre":"pan de barrra", "valor": 4500},
-            {"nombre":"pan frances", "valor": 7000}
-            {"nombre":}
+            {"nombre":"pan frances", "valor": 7000},
+            {"nombre":"pan de leche","valor":2500},
+            {"nombre":"pan de chapata","valor":13000},
+            {"nombre":"pan de viena","valor":12000},
+            {"nombre":"pan de hamburguesa","valor":9000},
+            ])
+        ([
+    "panes-integrales": {
+            "producto":list([
+                {"nombre":"pan integral", "valor":2700},
+                {"nombre":"pan de centeno integral", "valor":3000},
+                {"nombre":"pan multicereales","valor":2000},
+                {"nombre":"pan de espelta integral","valor":3500},
+                {"nombre":"pan de semillas","valor":2200},
+                {"nombre":"pan de trigo sarraceno","valor":7000},
+                {"nombre":"pan de masa madre integral","valor":4500},
+                {"nombre":"pan de nueces integral","valor":7800},
+                {"nombre":"pan de avena integral","valor":6500},
+                {"nombre":"pan de molde integral","valor":7800},
+                ])
+                ([
+    "panes especiales":{
+        "producto":list({})
+
+
+
+
+
         ])
+    }])
+                ])
+        }
     }
     "
 })
-Panes blancos:
-Baguette
-Pan de molde
-Pan de Viena
-Pan de pueblo
-Pan de barra
-Pan francés
-Pan de leche
-Pan de chapata
-Pan de viena
-Pan de hamburguesa
-Panes integrales:
-Pan integral
-Pan de centeno integral
-Pan multicereales
-Pan de espelta integral
-Pan de semillas
-Pan de trigo sarraceno
-Pan de masa madre integral
-Pan de nueces integral
-Pan de avena integral
+
 Pan de molde integral
 Otros panes especiales:
 Focaccia
